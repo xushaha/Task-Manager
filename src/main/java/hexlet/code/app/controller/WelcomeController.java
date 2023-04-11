1f2bc84159548e4d958e8e3152e3bc8facaf7ba9
xushaha <xushaha@ya.ru>
2023-04-11 19:52:43 +0300

    1st step part 3

---
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package hexlet.code.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String root() {
+        return "Welcome to Spring";
+    }
+}
